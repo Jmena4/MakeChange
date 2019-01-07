247dda2ad764ae72c66a413d7ca935d7ecf7a331
Jesse <Jesse.mena4@gmail.com>
2019-01-07 08:25:22 -0500

    add MakeChange file

---
+++ MakeChange.java
@@ -0,0 +1,98 @@
+package skilldistillery;
+
+import java.util.Scanner;
+
+public class MakeChange {
+
+
+	public static void main(String[] args) {
+
+		double itemPrice = 0;
+		double tendered = 0;
+		double remainder ;
+		//double changedOwed;
+		String change = "yes";
+		boolean go = true;
+
+		Scanner scanner = new Scanner(System.in);
+
+		// do loop that executes while the user does not type "no"
+		do {
+			// Prompting Cash Register user for transaction values itemPrice & tendered
+			System.out.println("What is the price of the item?");
+			itemPrice = scanner.nextDouble();
+
+			System.out.println("How much money was tendered by the customer?");
+			tendered = scanner.nextDouble();
+			// while go is true check each if statement for <, ==, >
+			while (go) {
+				if (tendered < itemPrice) {
+					tooLow();
+					go = false;
+				}
+				if (tendered == itemPrice) {
+					exact();
+					go = false;
+				}
+				if (tendered > itemPrice) {
+
+					remainder = tendered - itemPrice;
+					System.out.printf("\nResult: %.2f ", remainder  );
+					getChange(remainder);
+					go = false;
+				}
+			}
+			// loop here...
+			System.out.println("\nYou can try another sale: type \"yes\" to continue, \"no\" to exit\n");
+			change = scanner.next();
+			if (change.equals("yes")) {
+				go = true;
+			}
+		}
+		//exit loop by typing "no"
+		while (!change.equals("no"));
+			System.out.println("Finished.");
+			scanner.close();
+	}
+	//call the tooLow() method
+	public static void tooLow() {
+		System.out.println("Amount tendered is too low. ");
+	}
+	//call the exact() method
+	public static void exact() {
+		System.out.println("The customer provided the exact amount. No change required!");
+	}
+	//call the getChange() method and calculate the correct denominations to print out
+	public static void getChange(double changeOwed) {
+		double changeDenominations[] = {20.00 ,10.00 ,5.00 , 1.00, .25, .10, .05, .01};
+		String changeBills[]= {"twenty dollar bill", "ten dollar bill", "five dollar bill", "one dollar bill", "quater", "dime", "nickel", "penn"};
+		int counter;
+		double balance = changeOwed;
+		//counter continues until balance = 0
+		for ( int i = 0; i < changeDenominations.length; i++) {
+			counter = 0;
+			//while checks each cash denomination and subtracts the values from the balance
+			while (balance >= changeDenominations[i]) {
+				balance =  Math.round(100.0*(balance) )/100.0;
+				balance -= changeDenominations[i];
+				counter++;
+//				System.out.println(balance);
+				//  attempted to change the ending of
+				if (counter > 1 && changeBills[i] == "penn") {
+					changeBills[i] = changeBills[i] + "ies";
+
+				}
+				else if (counter == 1 && changeBills[i] == "penn") {
+					changeBills[i] = changeBills[i] + "y";
+				}
+//				if (counter >= 2 && changeBills[i] != "penn" ) {
+//					changeBills[i] = changeBills[i] + "s";
+//				}
+
+			}
+			if (counter != 0) {
+				System.out.print(counter + " " + changeBills[i] + "(s) " );
+			}
+		}
+	}
+}
