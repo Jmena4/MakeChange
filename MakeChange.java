21adb0a9bc05566599305093d23afa406ccfb921
Jesse <Jesse.mena4@gmail.com>
2019-01-09 09:20:16 -0500

    updated MakeChange.java

--- MakeChange.java
+++ MakeChange.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 
 public class MakeChange {
 
-
 	public static void main(String[] args) {
 
 		double itemPrice = 0;
 		double tendered = 0;
-		double remainder ;
-		//double changedOwed;
+		double remainder;
+
 		String change = "yes";
 		boolean go = true;
 
@@ -37,7 +36,7 @@ public class MakeChange {
 				if (tendered > itemPrice) {
 
 					remainder = tendered - itemPrice;
-					System.out.printf("\nResult: %.2f ", remainder  );
+					System.out.printf("\nResult: %.2f ", remainder);
 					getChange(remainder);
 					go = false;
 				}
@@ -49,49 +48,51 @@ public class MakeChange {
 				go = true;
 			}
 		}
-		//exit loop by typing "no"
+		// exit loop by typing "no"
 		while (!change.equals("no"));
-			System.out.println("Finished.");
-			scanner.close();
+		System.out.println("Finished.");
+		scanner.close();
 	}
-	//call the tooLow() method
+
+	// call the tooLow() method
 	public static void tooLow() {
 		System.out.println("Amount tendered is too low. ");
 	}
-	//call the exact() method
+
+	// call the exact() method
 	public static void exact() {
 		System.out.println("The customer provided the exact amount. No change required!");
 	}
-	//call the getChange() method and calculate the correct denominations to print out
+
+	// call the getChange() method and calculate the correct denominations to print
+	// out
 	public static void getChange(double changeOwed) {
-		double changeDenominations[] = {20.00 ,10.00 ,5.00 , 1.00, .25, .10, .05, .01};
-		String changeBills[]= {"twenty dollar bill", "ten dollar bill", "five dollar bill", "one dollar bill", "quater", "dime", "nickel", "penn"};
+		double changeDenominations[] = { 20.00, 10.00, 5.00, 1.00, .25, .10, .05, .01 };
+		String changeBills[] = { "twenty dollar bill", "ten dollar bill", "five dollar bill", "one dollar bill",
+				"quater", "dime", "nickel", "penn" };
 		int counter;
 		double balance = changeOwed;
-		//counter continues until balance = 0
-		for ( int i = 0; i < changeDenominations.length; i++) {
+//		boolean stop = true;
+		// counter continues until balance = 0
+		for (int i = 0; i < changeDenominations.length; i++) {
 			counter = 0;
-			//while checks each cash denomination and subtracts the values from the balance
+			// while checks each cash denomination and subtracts the values from the balance
 			while (balance >= changeDenominations[i]) {
-				balance =  Math.round(100.0*(balance) )/100.0;
+				balance = Math.round((balance)*100) / 100.0;
 				balance -= changeDenominations[i];
 				counter++;
 //				System.out.println(balance);
-				//  attempted to change the ending of
-				if (counter > 1 && changeBills[i] == "penn") {
+				if (counter != 0 && changeBills[i] == "penn") {
+					changeBills[i] = "penn" + "y";
+
+				}
+				else if (counter != 0 && counter >= 1 && changeBills[i] == "penn") {
 					changeBills[i] = changeBills[i] + "ies";
-
 				}
-				else if (counter == 1 && changeBills[i] == "penn") {
-					changeBills[i] = changeBills[i] + "y";
-				}
-//				if (counter >= 2 && changeBills[i] != "penn" ) {
-//					changeBills[i] = changeBills[i] + "s";
-//				}
-
 			}
-			if (counter != 0) {
-				System.out.print(counter + " " + changeBills[i] + "(s) " );
+			if (counter != 0 ) {
+				System.out.print(counter + " " + changeBills[i] + "(s) ");
+				
 			}
 		}
 	}
